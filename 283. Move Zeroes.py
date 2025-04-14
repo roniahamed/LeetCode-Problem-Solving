281352a2d9c95caf69508e80c9121492f96617c2
roniahamed <mdroniahamed56@gmail.com>
2025-04-14 11:51:28 +0600

    283. Move Zeroes

---
+++ 283. Move Zeroes.py
@@ -0,0 +1,7 @@
+class Solution:
+    def moveZeroes(self, nums: List[int]) -> None:
+        slow = 0
+        for fast in range(len(nums)):
+            if nums[fast] != 0:
+                nums[fast], nums[slow] = nums[slow], nums[fast]
+                slow += 1
