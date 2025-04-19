a5dbe7347322ec731d5d85d3eb1574740bf19ecf
roniahamed <mdroniahamed56@gmail.com>
2025-04-19 12:45:14 +0600

    142. Linked List Cycle II

---
+++ 142. Linked List Cycle II.py
@@ -0,0 +1,21 @@
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, x):
+#         self.val = x
+#         self.next = None
+
+class Solution:
+    def detectCycle(self, head: Optional[ListNode]) -> Optional[ListNode]:
+        slow = fast = pointer = head
+        test = {}
+        index = 0
+        while fast and fast.next:
+            slow = slow.next
+            fast = fast.next.next
+            if slow == fast:
+                while pointer != slow:
+                    pointer = pointer.next
+                    slow = slow.next
+                return pointer
+
+        return None
